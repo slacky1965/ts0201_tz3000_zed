7803fed3cc11923341b2eb06708b227284d82a12
Fedor <lfa1@yandex.ru>
2025-12-15 23:28:46 +0300

    added _TZ3000_dowj6gyi

--- zigbee2mqtt/ts0201_tz3000_orig.js
+++ zigbee2mqtt/ts0201_tz3000_orig.js
@@ -3,7 +3,8 @@ const {identify, reporting, ota} = require('zigbee-herdsman-converters/lib/moder
 const definition = {
     fingerprint: [{manufacturerName: '_TZ3000_xr3htd96'},
                   {manufacturerName: '_TZ3000_fllyghyj'},
-                  {manufacturerName: '_TZ3000_0s1izerx'}
+                  {manufacturerName: '_TZ3000_0s1izerx'},
+                  {manufacturerName: '_TZ3000_dowj6gyi'}
                  ],
     zigbeeModel: ['TS0201'],
     model: 'Original Tuya Temperature/Humidity sensor ready for update',
